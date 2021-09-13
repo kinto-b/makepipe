cf208075ce5a0a5eace5451a839e696329235d57
Kinto <kinto.behr@gmail.com>
2021-09-14 09:37:57 +1000

    Expand unit tests (#12)

--- tests/testthat/test-Pipeline.R
+++ tests/testthat/test-Pipeline.R
@@ -1,3 +1,6 @@
+
+# Invalidation -----------------------------------------------------------------
+
 test_that("Nodes correctly categorised as out-of-date", {
   source <- withr::local_tempfile(fileext = ".R")
   writeLines("2+2", source)
@@ -81,3 +84,85 @@ test_that("source files not invalidated", {
     )
   })
 })
+
+
+
+# Annotations ------------------------------------------------------------------
+withr::with_tempfile(c("dependency", "target"), code = {
+  saveRDS(iris, target)
+  saveRDS(iris, dependency)
+
+  set_pipeline(Pipeline$new())
+  make_with_recipe(
+    targets = target,
+    dependencies = dependency,
+    recipe = {2+2},
+    quiet = TRUE
+  )
+  pipe <- get_pipeline()
+
+  test_that("duplicate annotations are disallowed", {
+    annotation <- c("one", "two")
+    names(annotation) <- rep(dependency, 2)
+
+    expect_error(annotate_pipeline(
+      pipe,
+      tooltips = annotation,
+      labels = NULL
+    ), regexp = "must not be duplicated")
+
+    expect_error(annotate_pipeline(
+      pipe,
+      labels = annotation,
+      tooltips = NULL
+    ), regexp = "must not be duplicated")
+  })
+
+  # Non-existent
+  test_that("annotations cannot be applied to nodes that don't exist", {
+    expect_error(annotate_pipeline(
+      pipe,
+      tooltips = c("R/aaa.R" = "input"),
+      labels = NULL
+    ), regexp = "not nodes in `pipeline`")
+  })
+
+  # Non-character
+  test_that("annotations cannot must be character", {
+    tooltips <- c(1)
+    names(tooltips) <- dependency
+
+    expect_error(annotate_pipeline(
+      pipe,
+      tooltips = tooltips,
+      labels = NULL
+    ), regexp = "is.character")
+  })
+
+  test_that("annotations are stored correctly in nodes", {
+    annotation1 <- "annotation1"
+    names(annotation1) <- dependency
+
+    annotation2 <- "annotation2"
+    names(annotation2) <- target
+
+    x <- annotate_pipeline(
+      pipe,
+      tooltips = annotation1,
+      labels = annotation2
+    )
+    x <- x$nodes
+
+    expect_equal(
+      x[x$id %in% dependency, "title"],
+      "annotation1"
+    )
+
+    expect_equal(
+      x[x$id %in% target, "label"],
+      "annotation2"
+    )
+  })
+})
+
+
